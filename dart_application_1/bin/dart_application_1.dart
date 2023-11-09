832d07d65c7eace6bb0d7fb2fe72ebc3293e95d0
Gaspar Bianco <gasparbianco98@gmail.com>
2023-11-09 18:55:31 -0300

    variables y comentarios

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -2,4 +2,11 @@ import 'package:dart_application_1/dart_application_1.dart' as dart_application_
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
+
+  var num1 = 10;
+  var num2 = 2;
+  // Comentario
+  var sum = num1 + num2;
+  sum = sum + num1;
+  print(sum);
 }
